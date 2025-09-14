6e01a30e527e7df715d69c624ba20110b62c7141
abdullah201240 <abdullahalsakib7075@gmail.com>
2025-09-14 13:38:45 +0600

    new update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(",") || [],
+    domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(",") || ["images.unsplash.com"],
   },
 
   /* config options here */
